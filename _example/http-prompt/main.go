7b4cedd27357f879e59f3d1c506a642590b9f07a
Nathaniel Caldwell <nbcwell@gmail.com>
2022-08-17 19:26:46 -0400

    Cleanup more references

--- _example/http-prompt/main.go
+++ _example/http-prompt/main.go
@@ -10,7 +10,7 @@ import (
 	"path"
 	"strings"
 
-	prompt "github.com/c-bata/go-prompt"
+	prompt "github.com/koinos/go-prompt"
 )
 
 type RequestContext struct {
--- _tools/complete_file/main.go
+++ _tools/complete_file/main.go
@@ -5,8 +5,8 @@ import (
 	"os"
 	"strings"
 
-	prompt "github.com/c-bata/go-prompt"
-	"github.com/c-bata/go-prompt/completer"
+	prompt "github.com/koinos/go-prompt"
+	"github.com/koinos/go-prompt/completer"
 )
 
 var filePathCompleter = completer.FilePathCompleter{
